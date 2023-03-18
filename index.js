f70bca3c7c11e570d943c5c2d69a35dacd6b542f
TechBread <66066723+ovendev@users.noreply.github.com>
2023-03-18 12:31:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const Client = require('./src/Classes/Client');
+const Client = require('./src/Client');
 
-const BattleLogEndpoint = require('./src/Classes/Managers/BattleLogEndpoint');
-const BrawlerRankingsEndpoint = require('./src/Classes/Managers/BrawlerRankingsEndpoint');
-const BrawlerEndpoints = require('./src/Classes/Managers/BrawlersEnpoints');
-const ClubEndpoint = require('./src/Classes/Managers/ClubEndpoint');
-const ClubFromTag = require('./src/Classes/Managers/ClubFromTag');
-const ClubRankingsEndpoint = require('./src/Classes/Managers/ClubRankingsEndpoint');
-const PlayerFromTag = require('./src/Classes/Managers/PlayerFromTag');
-const PlayerRankingsEndpoint = require('./src/Classes/Managers/PlayerRankingsEndpoint');
-const PlayersEndpoints = require('./src/Classes/Managers/PlayersEndpoints');
-const RankingsEndpoints = require('./src/Classes/Managers/RankingsEndpoints');
-const RotationEndpoint = require('./src/Classes/Managers/RotationEndpoint');
+const BattleLogEndpoint = require('./src/Managers/BattleLogEndpoint');
+const BrawlerRankingsEndpoint = require('./src/Managers/BrawlerRankingsEndpoint');
+const BrawlerEndpoints = require('./src/Managers/BrawlersEnpoints');
+const ClubEndpoint = require('./src/Managers/ClubEndpoint');
+const ClubFromTag = require('./src/Managers/ClubFromTag');
+const ClubRankingsEndpoint = require('./src/Managers/ClubRankingsEndpoint');
+const PlayerFromTag = require('./src/Managers/PlayerFromTag');
+const PlayerRankingsEndpoint = require('./src/Managers/PlayerRankingsEndpoint');
+const PlayersEndpoints = require('./src/Managers/PlayersEndpoints');
+const RankingsEndpoints = require('./src/Managers/RankingsEndpoints');
+const RotationEndpoint = require('./src/Managers/RotationEndpoint');
 
-const BattleLog = require('./src/Classes/BattleLog');
-const Brawler = require('./src/Classes/Brawler');
-const Club = require('./src/Classes/Club');
-const Player = require('./src/Classes/Player');
-const RankedClub = require('./src/Classes/RankedClub');
-const RankedPlayer = require('./src/Classes/RankedPlayer');
-const RotatingEvent = require('./src/Classes/RotatingEvent');
+const BattleLog = require('./src/BattleLog');
+const Brawler = require('./src/Brawler');
+const Club = require('./src/Club');
+const Player = require('./src/Player');
+const RankedClub = require('./src/RankedClub');
+const RankedPlayer = require('./src/RankedPlayer');
+const RotatingEvent = require('./src/RotatingEvent');
 
 
 const TagResolver = require('./src/Functions/TagResolver');
